7eab04292847f622c9e42ba2ba59d55ca90b116c
Charles Armstrong <116760763+MonsterSquadChuck@users.noreply.github.com>
2022-10-26 14:58:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
+
+streamlit.body('Omega 3 & Blueberry Oatmeal')
+streamlit.body('Kale, Spinach and Rocket Smooth')
+streamlit.body('Hard-Boiled Free-Range Egg')
+           
