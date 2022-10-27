3f1d2ac8a52fd4150f909e8d69d7da17170d4296
Charles Armstrong <116760763+MonsterSquadChuck@users.noreply.github.com>
2022-10-27 09:21:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,10 +37,6 @@ try:
   streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
     streamlit.error()
-    
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
 
 # panda to fruityvice
 # frame
